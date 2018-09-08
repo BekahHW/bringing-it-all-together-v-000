cf5ba2c99fd6ef7d727e42ebbb07d3ffb2e64d53
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-08 23:18:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,10 +72,10 @@ class Dog
  end
 
 def self.new_from_db(row)
- new_dog = self.new  
- new_dog.id = id: row[0]
- new_dog.name =  name: row[1]
- new_dog.breed = breed: row[2]
+ new_dog = self.new(id: row[0], name: row[1], breed: row[2])  
+# new_dog.id = id: row[0]
+# new_dog.name =  name: row[1]
+# new_dog.breed = breed: row[2]
  new_dog  
 end
 # dog
